e5f5e00c614ded6389e8d92a511593683d5acece
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-05-26 14:26:23 +0530

    2 files modified

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import './style.css';
 
 
 // Write Javascript code!
-var obj=document.getElementById("cid");
-obj.innerText="#123456";
 var sobj=document.getElementById("cid");
 
 var btn=document.getElementById("btn1");
