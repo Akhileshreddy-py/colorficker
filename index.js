8541b73f87d81972899127dde32f2467b174884b
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-05-26 13:59:12 +0530

    2 files modified

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ import './style.css';
 // Write Javascript code!
 var obj=document.getElementById("cid");
 obj.innerText="#123456";
+
+var btn=document.getElementById("btn1");
+function colorchange()
+{
+  document.getElementsByTagName("body")[0].style.backgroundColor="#12f456";
+}
+btn.addEventListener(onclick,colorchange);
