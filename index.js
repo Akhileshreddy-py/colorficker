3703beed2ac7c3b99ee0cb23a270390dccbd6789
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-05-26 14:21:54 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 // Import stylesheets
 import './style.css';
 
+
 // Write Javascript code!
 var obj=document.getElementById("cid");
 obj.innerText="#123456";
+var sobj=document.getElementById("cid");
 
 var btn=document.getElementById("btn1");
 function ck()
 {
-  btn.innerText="wow man";
-  }
+
+var r=Math.random()*1000000;
+r=Math.floor(r);
+sobj.innerText="#"+r;
+  document.getElementById("bd").style.backgroundColor="#"+r;
+}
+
 //btn.addEventListener(onclick,ck);
-btn.addEventListener(onclick,ck)
+btn.addEventListener("click",ck)
